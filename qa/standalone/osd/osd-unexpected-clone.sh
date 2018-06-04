a7ada6c9c5425017693aef5c4c6ba7e5b930c094
Sage Weil <sage@redhat.com>
2018-06-04 12:51:11 -0500

    osd/PrimaryLogPG: fix on_local_recover crash on stray clone
    If there is a stray clone (one that does not appear in the SnapSet) and
    we do any sort of recovery on it the OSD will crash.  Log an error instead
    but continue.
    
    This addresses a problem where a cluster has both (1) an unexpected clone
    and (2) the clone is not present on all replicas.  Doing repair on that
    PG will both not fix the unexpected clone and also cause the remaining
    OSDs to crash trying to recover it.
    
    Include a test.
    
    Fixes: https://tracker.ceph.com/issues/24396
    Signed-off-by: Sage Weil <sage@redhat.com>

---
+++ qa/standalone/osd/osd-unexpected-clone.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2015 Intel <contact@intel.com.com>
+# Copyright (C) 2014, 2015 Red Hat <contact@redhat.com>
+#
+# Author: Xiaoxi Chen <xiaoxi.chen@intel.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Library Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library Public License for more details.
+#
+
+source $CEPH_ROOT/qa/standalone/ceph-helpers.sh
+
+function run() {
+    local dir=$1
+    shift
+
+    export CEPH_MON="127.0.0.1:7144" # git grep '\<7144\>' : there must be only one
+    export CEPH_ARGS
+    CEPH_ARGS+="--fsid=$(uuidgen) --auth-supported=none "
+    CEPH_ARGS+="--mon-host=$CEPH_MON "
+    CEPH_ARGS+="--osd-objectstore-fuse "
+
+    local funcs=${@:-$(set | sed -n -e 's/^\(TEST_[0-9a-z_]*\) .*/\1/p')}
+    for func in $funcs ; do
+        setup $dir || return 1
+        $func $dir || return 1
+        teardown $dir || return 1
+    done
+}
+
+function TEST_recover_unexpected() {
+    local dir=$1
+
+    run_mon $dir a || return 1
+    run_mgr $dir x || return 1
+    run_osd $dir 0 || return 1
+    run_osd $dir 1 || return 1
+    run_osd $dir 2 || return 1
+
+    ceph osd pool create foo 1
+    rados -p foo put foo /etc/passwd
+    rados -p foo mksnap snap
+    rados -p foo put foo /etc/motd
+
+    find $dir
+    cp $dir/1/fuse/1.0_head/all/#1\:602f83fe\:\:\:foo\:1#/attr/_ _
+    cp $dir/1/fuse/1.0_head/all/#1\:602f83fe\:\:\:foo\:1#/data data
+
+    rados -p foo rmsnap snap
+
+    sleep 5
+
+    primary=1
+    mkdir $dir/$primary/fuse/1.0_head/all/#1\:602f83fe\:\:\:foo\:1#
+    cat data > $dir/$primary/fuse/1.0_head/all/#1\:602f83fe\:\:\:foo\:1#/data
+    cat _ > $dir/$primary/fuse/1.0_head/all/#1\:602f83fe\:\:\:foo\:1#/attr/_
+
+    sleep 5
+
+    ceph pg scrub 1.0
+    ceph pg repair 1.0
+
+    sleep 10
+
+    # make sure osds are still up
+    ceph tell osd.0 version
+    ceph tell osd.1 version
+    ceph tell osd.2 version
+}
+
+
+main osd-unexpected-clone "$@"
+
+# Local Variables:
+# compile-command: "cd ../.. ; make -j4 && test/osd/osd-bench.sh"
+# End:
