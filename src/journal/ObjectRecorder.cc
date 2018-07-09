1e65199fd4e91c0a4d667cfc76bc7fb838b27c45
Mykola Golub <mgolub@suse.com>
2018-07-09 15:32:13 +0300

    journal: limit in-flight appends
    Signed-off-by: Mykola Golub <mgolub@suse.com>

--- src/journal/ObjectRecorder.cc
+++ src/journal/ObjectRecorder.cc
@@ -22,12 +22,14 @@ ObjectRecorder::ObjectRecorder(librados::IoCtx &ioctx, const std::string &oid,
                                ContextWQ *work_queue, SafeTimer &timer,
                                Mutex &timer_lock, Handler *handler,
                                uint8_t order, uint32_t flush_interval,
-                               uint64_t flush_bytes, double flush_age)
+                               uint64_t flush_bytes, double flush_age,
+                               uint64_t max_in_flight_appends)
   : RefCountedObject(NULL, 0), m_oid(oid), m_object_number(object_number),
     m_cct(NULL), m_op_work_queue(work_queue), m_timer(timer),
     m_timer_lock(timer_lock), m_handler(handler), m_order(order),
     m_soft_max_size(1 << m_order), m_flush_interval(flush_interval),
-    m_flush_bytes(flush_bytes), m_flush_age(flush_age), m_flush_handler(this),
+    m_flush_bytes(flush_bytes), m_flush_age(flush_age),
+    m_max_in_flight_appends(max_in_flight_appends), m_flush_handler(this),
     m_lock(lock), m_append_tid(0), m_pending_bytes(0),
     m_size(0), m_overflowed(false), m_object_closed(false),
     m_in_flight_flushes(false), m_aio_scheduled(false) {
@@ -99,6 +101,8 @@ void ObjectRecorder::flush(Context *on_safe) {
       future = Future(m_append_buffers.rbegin()->first);
 
       flush_appends(true);
+    } else if (!m_pending_buffers.empty()) {
+      future = Future(m_pending_buffers.rbegin()->first);
     } else if (!m_in_flight_appends.empty()) {
       AppendBuffers &append_buffers = m_in_flight_appends.rbegin()->second;
       assert(!append_buffers.empty());
@@ -263,6 +267,7 @@ void ObjectRecorder::handle_append_flushed(uint64_t tid, int r) {
 
       // notify of overflow once all in-flight ops are complete
       if (m_in_flight_tids.empty() && !m_aio_scheduled) {
+        m_append_buffers.splice(m_append_buffers.begin(), m_pending_buffers);
         append_overflowed();
         notify_handler_unlock();
       } else {
@@ -293,9 +298,16 @@ void ObjectRecorder::handle_append_flushed(uint64_t tid, int r) {
   m_in_flight_flushes = false;
   m_in_flight_flushes_cond.Signal();
 
-  if (m_in_flight_appends.empty() && !m_aio_scheduled && m_object_closed) {
-    // all remaining unsent appends should be redirected to new object
-    notify_handler_unlock();
+  if (!m_aio_scheduled) {
+    if (m_in_flight_appends.empty() && m_object_closed) {
+      // all remaining unsent appends should be redirected to new object
+      m_append_buffers.splice(m_append_buffers.begin(), m_pending_buffers);
+      notify_handler_unlock();
+    } else {
+      m_aio_scheduled = true;
+      m_lock->Unlock();
+      send_appends_aio();
+    }
   } else {
     m_lock->Unlock();
   }
@@ -357,44 +369,57 @@ void ObjectRecorder::send_appends(AppendBuffers *append_buffers) {
 }
 
 void ObjectRecorder::send_appends_aio() {
-  librados::ObjectWriteOperation op;
-  client::guard_append(&op, m_soft_max_size);
-  C_Gather *gather_ctx;
-  {
-    Mutex::Locker locker(*m_lock);
-    uint64_t append_tid = m_append_tid++;
-    m_in_flight_tids.insert(append_tid);
+  m_lock->Lock();
+  m_aio_scheduled = false;
 
-    ldout(m_cct, 10) << __func__ << ": " << m_oid << " flushing journal tid="
-                     << append_tid << dendl;
-
-    gather_ctx = new C_Gather(m_cct, new C_AppendFlush(this, append_tid));
-    auto append_buffers = &m_in_flight_appends[append_tid];
-
-    for (auto it = m_pending_buffers.begin(); it != m_pending_buffers.end(); ) {
-      ldout(m_cct, 20) << __func__ << ": flushing " << *it->first << dendl;
-      op.append(it->second);
-      op.set_op_flags2(CEPH_OSD_OP_FLAG_FADVISE_DONTNEED);
-      m_aio_sent_size += it->second.length();
-      append_buffers->push_back(*it);
-      it = m_pending_buffers.erase(it);
-      if (m_aio_sent_size >= m_soft_max_size) {
-        break;
-      }
-    }
+  if (m_pending_buffers.empty()) {
+    ldout(m_cct, 20) << __func__ << ": " << m_oid << " pending buffers empty"
+                     << dendl;
+    m_lock->Unlock();
+    return;
   }
 
+  if (m_max_in_flight_appends != 0 &&
+      m_in_flight_tids.size() >= m_max_in_flight_appends) {
+    ldout(m_cct, 20) << __func__ << ": " << m_oid
+                     << " max in flight appends reached" << dendl;
+    m_lock->Unlock();
+    return;
+  }
+
+  uint64_t append_tid = m_append_tid++;
+  m_in_flight_tids.insert(append_tid);
+
+  ldout(m_cct, 10) << __func__ << ": " << m_oid << " flushing journal tid="
+                   << append_tid << dendl;
+
+  librados::ObjectWriteOperation op;
+  client::guard_append(&op, m_soft_max_size);
+  auto append_buffers = &m_in_flight_appends[append_tid];
+
+  for (auto it = m_pending_buffers.begin(); it != m_pending_buffers.end(); ) {
+    ldout(m_cct, 20) << __func__ << ": flushing " << *it->first << dendl;
+    op.append(it->second);
+    op.set_op_flags2(CEPH_OSD_OP_FLAG_FADVISE_DONTNEED);
+    m_aio_sent_size += it->second.length();
+    append_buffers->push_back(*it);
+    it = m_pending_buffers.erase(it);
+    if (m_aio_sent_size >= m_soft_max_size) {
+      break;
+    }
+  }
+  m_lock->Unlock();
+
   librados::AioCompletion *rados_completion =
-    librados::Rados::aio_create_completion(gather_ctx->new_sub(), nullptr,
-                                           utils::rados_ctx_callback);
+    librados::Rados::aio_create_completion(new C_AppendFlush(this, append_tid),
+                                           nullptr, utils::rados_ctx_callback);
   int r = m_ioctx.aio_operate(m_oid, rados_completion, &op);
   assert(r == 0);
   rados_completion->release();
 
-  {
-    m_lock->Lock();
+  m_lock->Lock();
+  if (!m_aio_scheduled) {
     if (m_pending_buffers.empty()) {
-      m_aio_scheduled = false;
       if (m_in_flight_appends.empty() && m_object_closed) {
         // all remaining unsent appends should be redirected to new object
         notify_handler_unlock();
@@ -404,14 +429,14 @@ void ObjectRecorder::send_appends_aio() {
     } else {
       // additional pending items -- reschedule
       m_op_work_queue->queue(new FunctionContext([this] (int r) {
-          send_appends_aio();
-        }));
+            send_appends_aio();
+          }));
+      m_aio_scheduled = true;
       m_lock->Unlock();
     }
+  } else {
+    m_lock->Unlock();
   }
-
-  // allow append op to complete
-  gather_ctx->activate();
 }
 
 void ObjectRecorder::notify_handler_unlock() {
